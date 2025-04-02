c04ff500e30a6cae97efcce61e86b4512e3651f2
fufu.fuf <fufu.fuf@bytedance.com>
2025-04-02 17:18:42 +0800

    change README.md

---
+++ test2.py
@@ -0,0 +1,20 @@
+def test():
+    print('test')
+    a =1
+    test2()
+    return 'test'
+def test2():
+    a=2
+    print('test2')
+    return 'test2'
+def test():
+    print('test')
+if __name__ == '__main__':
+    test()
+    print('test.py')
+    test2()
+    test()
+#test2.py
+#sdfsfsdfsdfsdf
+#sdfsdfsdf
+def asdfa():
